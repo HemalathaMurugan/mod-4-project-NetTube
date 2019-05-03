39f52944f457ca302594418b0999de7e871e418d
HemalathaMurugan <hemalatha.psna@gmail.com>
2019-05-03 16:22:16 -0500

    seeded username and passwords

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(version: 2019_05_02_162444) do
 
   create_table "users", force: :cascade do |t|
     t.string "user_name"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
