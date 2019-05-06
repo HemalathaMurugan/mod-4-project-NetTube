48dd78e13c69c29ca74b7033409f8320555847ef
HemalathaMurugan <hemalatha.psna@gmail.com>
2019-05-06 10:52:38 -0500

    authentication partally works yet to fix

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -7,11 +7,57 @@ class ApplicationController < ActionController::Base
 
         begin 
             token = request.headers['Authorization'].split(' ')[1]
-            # type, token = request.headers['Authorization'].split(' ')
+          
             
             payload = JWT.decode(token, '5ebe2294ecd0e0f08eab7690d2a6ee69')[0]
 
-            # payload, header = JWT.decode(token, '5ebe2294ecd0e0f08eab7690d2a6ee69')
+            before_action :define_current_dragon
+
+            def create
+                dragon = Dragon.create(dragon_params)
+                render json: dragon
+            end
+        
+            def index
+                render json: Dragon.all
+            end
+        
+            def show
+                render json: current_dragon
+            end
+        
+            def update
+                if current_user == current_dragon.user
+                    current_dragon.update(dragon_params)
+                    render json: current_dragon
+                else
+                    render json: {
+                        error: true,
+                        message: 'That is not your dragon'
+                    }
+                end
+            end
+        
+            def destroy
+                current_dragon.destroy
+                render json: current_dragon
+            end
+        
+            def dragon_params
+                params.permit(:name, :image, :description, :atWar)
+            end
+        
+            def define_current_dragon
+                if params[:id]
+                    @current_dragon = Dragon.find(params[:id])
+                else
+                    @current_dragon = Dragon.new
+                end
+            end
+        
+            def current_dragon
+                @current_dragon
+            end
 
             @current_user = User.find(payload['id'])
 
@@ -24,7 +70,7 @@ class ApplicationController < ActionController::Base
                 }
             end
         rescue 
-            # They are not
+           
             render json: {
                 error: true,
                 message: 'Invalid Authentication'
