62f87b4774cbcf084d816ffec4fa42cdc4a8694a
HemalathaMurugan <hemalatha.psna@gmail.com>
2019-05-06 12:54:16 -0500

    fixed merge for backend

--- server/app/controllers/videos_controller.rb
+++ server/app/controllers/videos_controller.rb
@@ -1,36 +1,49 @@
 class VideosController < ApplicationController
-    def index
-        render json: Video.all,
-    end
-
-    def show
-        render json: Video.find(params[:id])
-    end
+    before_action :define_current_video
 
     def create
         video = Video.create(video_params)
         render json: video
     end
 
-    def update
-        video = Video.find(params[:id])
-        video.update(video_params)
-        video.save
-        render json: video
+    def index
+        render json: Video.all
     end
 
-    def destroy
-        video = Video.find(params[:id])
-        unless video.nil?
-            video.destroy
-            render json: video
+    def show
+        render json: current_video
+    end
+
+    def update
+        if current_user == current_video.user
+            current_video.update(video_params)
+            render json: current_video
         else
-            render json: { error: "No video found!" }, status: 404
+            render json: {
+                error: true,
+                message: 'That is not your video'
+            }
         end
     end
 
-    private
+    def destroy
+        current_video.destroy
+        render json: current_video
+    end
+
     def video_params
-        params.require(:video).permit(:user_id, :title, :description, :likes, :duration, :created_at)
+        params.permit(:title, :user_id, :description, :duration)
+    end
+
+    def define_current_video
+        if params[:id]
+            @current_video = Video.find(params[:id])
+        else
+            @current_video = Video.new
+        end
+    end
+
+    def current_video
+        @current_video
     end
 end
--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :comments
   resources :videos
   resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # get '/login', to: 'users#authenticate'
+  post '/login', to: 'users#authenticate'
 end
