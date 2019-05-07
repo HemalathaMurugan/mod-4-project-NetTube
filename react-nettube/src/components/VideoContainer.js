b96334b60cdd6f8e13bf65d4571899cc33ef78fb
josieyao <yao.josephine@yahoo.com>
2019-05-07 17:59:28 -0500

    Abstracted video components

--- react-nettube/src/components/VideoContainer.js
+++ react-nettube/src/components/VideoContainer.js
@@ -1,16 +1,23 @@
 import React from 'react'
 import VideoList from './VideoList'
-import Filter from './Filter'
 
 export default class VideoContainer extends React.Component {
 
     render(){
-        return(
-            <div className="video-container">
-                <h1>Video Container</h1>
-                <Filter filterVideos={this.props.filterVideos}/>
-                <VideoList videos={this.props.videos} filteredVideos={this.props.filteredVideos} filter={this.props.filter}/>
-            </div>
-        )
+        if (this.props.filter === "All") {
+            return (
+                <div className="video-container">
+                    {/* <VideoList videos={this.props.videos} filteredVideos={this.props.filteredVideos} filter={this.props.filter} /> */}
+                    <VideoList videos={this.props.videos.filter(video => video.language === "Javascript")} title="Javascript"/>
+                    <VideoList videos={this.props.videos.filter(video => video.language === "Ruby")} title="Ruby" />
+                    <VideoList videos={this.props.videos.filter(video => video.language === "React")} title="React"/>
+                    <VideoList videos={this.props.videos.filter(video => video.language === "Rails")} title="Rails"/>
+                </div>
+            )
+        } else {
+            return (
+                <VideoList videos={this.props.videos.filter(video => video.language === this.props.filter)} title={this.props.filter}/>
+            )
+        }
     }
 }
