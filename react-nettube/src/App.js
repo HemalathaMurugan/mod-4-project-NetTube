018186e5b8cbd2a8ad8c44c2c23268b51fdc7484
HemalathaMurugan <hemalatha.psna@gmail.com>
2019-05-08 12:20:29 -0500

    -

--- react-nettube/src/App.js
+++ react-nettube/src/App.js
@@ -4,12 +4,9 @@ import Main from './components/Main'
 import Login from './components/Login'
 import Video from './components/Video'
 import User from './components/User'
-<<<<<<< HEAD
 import NewUser from './components/NewUser'
 import NewVideoForm from './components/NewVideoForm'
-=======
 import EditVideo from './components/EditVideo'
->>>>>>> new
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
@@ -32,14 +29,11 @@ function App() {
           <Route path="/new" component={NewUser} />
           <Route exact path="/" render={() => <Main videos={videos}/>}/>
           <Route path="/video" component={Video}/>
-<<<<<<< HEAD
           <Route path="/add-video" component={NewVideoForm}/>
           <Route path="/user" render={() => <User videos={videos}/>} />
           {/* <Route path={`/user/${user.id}/edit}`} component={EditUser} */}
-=======
-          <Route path="/user" component={User}/>
+          {/* <Route path="/user" component={User}/> */}
           <Route path="/edit-video/:id" component={EditVideo} />
->>>>>>> new
         </Switch>
       </div>
     </Router>
